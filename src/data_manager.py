91b5f02f94de9140ac414f3b7a27c7e8cf53c049
Daniel Maldonado <damarias991@gmail.com>
2025-03-15 23:37:59 -0500

    refactor: implement OOP for API interaction

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,3 +1,30 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.base_url = os.getenv("BASE_SHEETY_URL")
+        self.auth_token = os.getenv("SHEETY_AUTH_TOKEN")
+        self.destination_data = {}
+        
+        if not self.base_url or not self.auth_token:
+            raise ValueError("Missing environment variables")
+        
+    def get_data(self) -> dict:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.auth_token}"
+        }
+
+        try:
+            response = requests.get(url=self.base_url, headers=headers)
+            response.raise_for_status()
+            self.destination_data = response.json()    
+            return self.destination_data
+        except requests.exceptions.HTTPError as e:
+            raise RuntimeError(f"Error fetching data: {e}") from e
+
+
