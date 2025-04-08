fb03b0ee65ac089819315255bd440329a7b3f568
Daniel Maldonado <damarias991@gmail.com>
2025-04-08 05:23:28 -0500

    feat: update cities with missing IATA code using update_destination_codes

--- src/data_manager.py
+++ src/data_manager.py
@@ -2,10 +2,11 @@ import os
 from dotenv import load_dotenv
 import requests
 
-load_dotenv()
+load_dotenv() # loads environment variables from .env file
 
 class DataManager:
     def __init__(self):
+        """Reads the base Sheety API URL and token. Prepares authorization headers. Initializes two empty data dicts. Validates that required environment variables exist."""
         self.base_url = os.getenv("BASE_SHEETY_URL")
         self.auth_token = os.getenv("SHEETY_AUTH_TOKEN")
         self.headers = {
@@ -19,7 +20,7 @@ class DataManager:
             raise ValueError("Missing environment variables")
         
     def get_data(self) -> dict:
-
+        """Performs a GET request to the Sheety API. Stores the result in self.destination_data. Returns the JSON payload."""
         try:
             response = requests.get(url=self.base_url, headers=self.headers)
             response.raise_for_status()
@@ -28,16 +29,31 @@ class DataManager:
         except requests.exceptions.HTTPError as e:
             raise RuntimeError(f"Error fetching data: {e}") from e
         
-    def update_data(self) -> str:
-        for row in self.destination_data["sheet1"]:
-            row_id = row['id']
-            try:
-                put_url = f"{self.base_url}/{row_id}"  # Build PUT request URL
-                put_response = requests.put(url=put_url, headers=self.headers, json=self.update_data)
-                put_response.raise_for_status()
-            except requests.exceptions.HTTPError as e:
-                raise RuntimeError(f"Error updating data: {e}")
-        
+    def update_destination_codes(self) -> str:
+        """Iterates through the destination_data object(?)."""
+        for city in self.destination_data["sheet1"]:
+            row_id = city['id']
+            
+            # Check if IATA code is empty
+            if city['iataCode'] == '':
+                new_data = {
+                    "sheet1": {
+                        "iataCode": "TESTING"
+                    }
+                }
+            
+                try:
+                    put_url = f"{self.base_url}/{row_id}"  # Build PUT request URL
+                    put_response = requests.put(url=put_url, headers=self.headers, json=new_data)
+                    put_response.raise_for_status()
+                except requests.exceptions.HTTPError as e:
+                    raise RuntimeError(f"Error updating data: {e}")
+            
+            
+if __name__ == "__main__":
+        data_manager = DataManager()
+        data = data_manager.get_data()
+        data_manager.update_destination_codes()
         
         
 
